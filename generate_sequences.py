4c3a96519c747c88982617a9b65c525ff7efe736
Jeremy Tang <jtang56@hotmail.com>
2018-05-02 12:57:07 -0500

    Generating Sequences

--- generate_sequences.py
+++ generate_sequences.py
@@ -4,52 +4,43 @@ import numpy as np
 from numpy.random import rand
 #from sampling import sample
 
-def sample(alphabet, dist):
-	sampl = None
+def sampler(alphabet, dist):
+	sample = None
 	cum_dist = np.cumsum(dist)
 	r = rand()
 	for i in xrange(len(dist)):
 		if r < cum_dist[i]:
-			sampl = alphabet[i]
+			sample = alphabet[i]
 			break
-	return sampl
+	return sample
 
-### The properties for the generated sequence
+K = 10
+N = 150
+w = 6
 
-K = 10	 # The number of sequence
-N = 150  # The length for each sequence
-w = 6	# The length of the magic word
+alphabet = ['A', 'C', 'T', 'G']	
+M = len(alphabet)
 
-alphabet = ['A', 'C', 'T', 'G']			# The alphabet used in the sequence
-M = len(alphabet)						# The number of alphabet used
+alpha_b = [1] * M
+alpha_w = [10,2,8,3]
 
-alpha_b = [1]*M				# The alpha parameter for dirichlet prior of background letter
-alpha_w = [10,2,8,3]		 # The alpha parameter for dirichlet prior of hidden word
-
-
-### Start the generator part
-
-# First, generate the starting position of the magic word for all sequences uniformly
-position = [0]*K
+position = [0] * K
 for i in xrange(K):
-	position[i] = randint(0, N-w+1)
+	position[i] = randint(0, N - w+1)
 	
-# Generate the background letters for all sequences
 cat_b = dirichlet(alpha_b)
 sequences = []
 for i in xrange(K):
 	seq = []
 	for j in xrange(N):
-		seq += [sample(alphabet, cat_b)]
+		seq += [sampler(alphabet, cat_b)]
 	sequences += [seq]
 
-# Generate the magic words
 theta = dirichlet(alpha_w, w)
 for i in xrange(K):
 	start_pos = position[i]
 	for j in xrange(w):
-		sequences[i][start_pos+j] = sample(alphabet, theta[j])
-
+		sequences[i][start_pos+j] = sampler(alphabet, theta[j])
 
 filename = 'sequences.fa'
 
